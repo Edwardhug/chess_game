e32bf6ba2b6b4bd25a0b7be8d5728f259d334a55
root <root@DESKTOP-7CO9QQK>
2023-07-08 20:05:42 +0200

    check if the party is finish

--- game/main_chess.py
+++ game/main_chess.py
@@ -2,15 +2,15 @@
 
 import chess
 
-# board = chess.Board()
-# board = chess.Board("k7/8/8/8/8/8/5q2/7K")		#stalemate situation
-# board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")
+board = chess.Board()
+# board = chess.Board("k7/8/8/8/8/8/5q2/7K")													 #stalemate situation
+# board = chess.Board("k7/8/8/8/8/8/8/7K")														 #pat no check mate possible
+# board = chess.Board("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4")      #check mate
 
 print(board)
-if (board.is_stalemate() == True):		#Pat Checker with no move possible
+if (board.is_stalemate() == True):
 	print("Pat by stalemate")
-# if (board.outcome() == Outcome(termination=<Termination.CHECKMATE: 1>, winner=True)):
-# 	print("Check mate")
-
-board.outcome()
-# Outcome(termination=<Termination.CHECKMATE: 1>, winner=True)
+if (board.is_insufficient_material() == True):
+	print("Pat : no way to end the game by check mate")
+if (board.is_checkmate() == True):
+	print("Check mate")
