03e2f2e39eaf9567a6704ea4afe5485764e7573b
root <root@DESKTOP-7CO9QQK>
2023-07-11 17:18:46 +0200

    grid printing works

--- game/main_chess.py
+++ game/main_chess.py
@@ -5,6 +5,8 @@ import pygame
 import time
 import sys
 
+# ---------------------------------------creating pieces class ----------------------
+
 class Piece:
 	def __init__(self, type, image, killable=False):
 		self.type = type
@@ -25,54 +27,72 @@ bc = Piece('C', 'Pieces_png/cb.png')
 wc = Piece('c', 'Pieces_png/cw.png')
 
 starting_order = {(0, 0): pygame.image.load(br.image), (1, 0): pygame.image.load(bc.image),
-                  (2, 0): pygame.image.load(bb.image), (3, 0): pygame.image.load(bk.image),
-                  (4, 0): pygame.image.load(bq.image), (5, 0): pygame.image.load(bb.image),
-                  (6, 0): pygame.image.load(bc.image), (7, 0): pygame.image.load(br.image),
-                  (0, 1): pygame.image.load(bp.image), (1, 1): pygame.image.load(bp.image),
-                  (2, 1): pygame.image.load(bp.image), (3, 1): pygame.image.load(bp.image),
-                  (4, 1): pygame.image.load(bp.image), (5, 1): pygame.image.load(bp.image),
-                  (6, 1): pygame.image.load(bp.image), (7, 1): pygame.image.load(bp.image),
+				  (2, 0): pygame.image.load(bb.image), (3, 0): pygame.image.load(bk.image),
+				  (4, 0): pygame.image.load(bq.image), (5, 0): pygame.image.load(bb.image),
+				  (6, 0): pygame.image.load(bc.image), (7, 0): pygame.image.load(br.image),
+				  (0, 1): pygame.image.load(bp.image), (1, 1): pygame.image.load(bp.image),
+				  (2, 1): pygame.image.load(bp.image), (3, 1): pygame.image.load(bp.image),
+				  (4, 1): pygame.image.load(bp.image), (5, 1): pygame.image.load(bp.image),
+				  (6, 1): pygame.image.load(bp.image), (7, 1): pygame.image.load(bp.image),
 
-                  (0, 2): None, (1, 2): None, (2, 2): None, (3, 2): None,
-                  (4, 2): None, (5, 2): None, (6, 2): None, (7, 2): None,
-                  (0, 3): None, (1, 3): None, (2, 3): None, (3, 3): None,
-                  (4, 3): None, (5, 3): None, (6, 3): None, (7, 3): None,
-                  (0, 4): None, (1, 4): None, (2, 4): None, (3, 4): None,
-                  (4, 4): None, (5, 4): None, (6, 4): None, (7, 4): None,
-                  (0, 5): None, (1, 5): None, (2, 5): None, (3, 5): None,
-                  (4, 5): None, (5, 5): None, (6, 5): None, (7, 5): None,
+				  (0, 2): None, (1, 2): None, (2, 2): None, (3, 2): None,
+				  (4, 2): None, (5, 2): None, (6, 2): None, (7, 2): None,
+				  (0, 3): None, (1, 3): None, (2, 3): None, (3, 3): None,
+				  (4, 3): None, (5, 3): None, (6, 3): None, (7, 3): None,
+				  (0, 4): None, (1, 4): None, (2, 4): None, (3, 4): None,
+				  (4, 4): None, (5, 4): None, (6, 4): None, (7, 4): None,
+				  (0, 5): None, (1, 5): None, (2, 5): None, (3, 5): None,
+				  (4, 5): None, (5, 5): None, (6, 5): None, (7, 5): None,
 
-                  (0, 6): pygame.image.load(wp.image), (1, 6): pygame.image.load(wp.image),
-                  (2, 6): pygame.image.load(wp.image), (3, 6): pygame.image.load(wp.image),
-                  (4, 6): pygame.image.load(wp.image), (5, 6): pygame.image.load(wp.image),
-                  (6, 6): pygame.image.load(wp.image), (7, 6): pygame.image.load(wp.image),
-                  (0, 7): pygame.image.load(wr.image), (1, 7): pygame.image.load(wc.image),
-                  (2, 7): pygame.image.load(wb.image), (3, 7): pygame.image.load(wk.image),
-                  (4, 7): pygame.image.load(wq.image), (5, 7): pygame.image.load(wb.image),
-                  (6, 7): pygame.image.load(wc.image), (7, 7): pygame.image.load(wr.image),}
+				  (0, 6): pygame.image.load(wp.image), (1, 6): pygame.image.load(wp.image),
+				  (2, 6): pygame.image.load(wp.image), (3, 6): pygame.image.load(wp.image),
+				  (4, 6): pygame.image.load(wp.image), (5, 6): pygame.image.load(wp.image),
+				  (6, 6): pygame.image.load(wp.image), (7, 6): pygame.image.load(wp.image),
+				  (0, 7): pygame.image.load(wr.image), (1, 7): pygame.image.load(wc.image),
+				  (2, 7): pygame.image.load(wb.image), (3, 7): pygame.image.load(wk.image),
+				  (4, 7): pygame.image.load(wq.image), (5, 7): pygame.image.load(wb.image),
+				  (6, 7): pygame.image.load(wc.image), (7, 7): pygame.image.load(wr.image),}
+
+
+# ------------------------Fonction to print grid---------------------------------------------
+
+def print_grid(WIDTH, WHITE, WIN):
+	i = 0
+	while i < 8:
+		j = 0
+		while j < 8:
+			pygame.draw.rect(WIN, WHITE, pygame.Rect(j * (WIDTH / 8), i * (WIDTH / 8), WIDTH / 8, WIDTH / 8))
+			j += 2
+		i += 1
+		j = 1
+		while j < 8:
+			pygame.draw.rect(WIN, WHITE, pygame.Rect(j * (WIDTH / 8), i * (WIDTH / 8), WIDTH / 8, WIDTH / 8))
+			j += 2
+		i += 1
+	pygame.display.flip()
 
 
 # -----------------------Init window and looping it------------------------------------------
 
 
 WIDTH = 800
+done = False
+WHITE = (250,235,215)
 
 pygame.init()
-
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-
-done = False
-
 keys=pygame.key.get_pressed()
+pygame.display.set_caption("Chess")
+print_grid(WIDTH, WHITE, WIN)
 
 while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            keys = pygame.key.get_pressed()
-    
-    if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
-        done = True
+	# --- Main event loop
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+			keys = pygame.key.get_pressed()
+	
+	if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
+		done = True
 
 pygame.quit()
 
